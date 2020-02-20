c7081f6bd155913c4fbfa7690165743d51e71b31
ryan <ryan.jo@medibloc.org>
2020-02-20 21:47:27 +0900

    test swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+import PackageDescription
+
+let package = Package(
+    name: "secp256k1",
+    products: [
+        .library(name: "secp256k1", targets: ["secp256k1"]),
+    ],
+    targets: [
+        .target(
+            name: "secp256k1",
+            path: ".",
+            sources: ["src/secp256k1.c"],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("src"),
+                .headerSearchPath("."),
+                .define("ENABLE_MODULE_ECDH"),
+                .define("ENABLE_MODULE_RECOVERY"),
+                .define("USE_NUM_NONE"),
+                .define("USE_FIELD_INV_BUILTIN"),
+                .define("USE_SCALAR_INV_BUILTIN"),
+                .define("USE_FIELD_10X26"),
+                .define("USE_SCALAR_8X32"),
+                .define("ECMULT_GEN_PREC_BITS", to: "4"),
+                .define("ECMULT_WINDOW_SIZE", to: "15"),
+            ]
+        ),
+    ]
+)
